1595474bc37eb7320c262bc35bf1f163d69cc2b1
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2019-09-11 10:50:29 -0300

    Finish healthcheck implementation

--- domain/healthcheck.rb
+++ domain/healthcheck.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 module Domain
   class Healthcheck
-    def self.call
-      { status: Models::Beneficio.count }
+    class << self
+      def call
+        {
+          versao: 1,
+          tag_versao: TAG_VERSAO,
+          servicos: {
+            banco: try { Models::Beneficio.select(:id).limit(1) }
+          }
+        }
+      end
+
+      private
+
+      def try
+        {
+          status: :ok,
+          time: response_time { yield }
+        }
+      rescue StandardError
+        @fail = true
+        { status: :not_ok }
+      end
+
+      def response_time
+        tempo = Benchmark.realtime { yield }
+        (tempo * 1000).round(2) # conversão do tempo para milisengundos
+      end
     end
   end
 end
