efca952a8daeb42179c23954d08816fa68e3692e
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2019-09-11 10:49:25 -0300

    Finish seeds implementation and adjust migrations

--- db/migrations/20190909191512_cria_tabela_beneficios.rb
+++ db/migrations/20190909191512_cria_tabela_beneficios.rb
@@ -3,10 +3,15 @@
 Sequel.migration do
   up do
     create_table(:beneficios) do
-      primary_key :id
+      Integer :id
       String :nome, null: false
       String :descricao
       TrueClass :ativo, null: false, default: true
+    end
+
+    alter_table(:beneficios) do
+      add_index :id, name: :IDX_beneficio_id, unique: true
+      add_index :nome, name: :IDX_beneficio_nome, unique: true
     end
   end
 
