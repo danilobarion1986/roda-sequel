90c5b39c887f796185e4ca0aa2df79f573795023
Danilo Barion Nogueira <danilo.barion@gmail.com>
2020-04-10 00:20:40 -0300

    Update template migration

--- db/migrations/20190909191512_create_your_table.rb
+++ db/migrations/20190909191512_create_your_table.rb
@@ -2,7 +2,7 @@
 
 Sequel.migration do
   up do
-    create_table(:your_table) do
+    create_table(:first_table) do
       Integer :id
       String :name, null: false
       String :description
@@ -10,6 +10,6 @@ Sequel.migration do
   end
 
   down do
-    drop_table(:your_table)
+    drop_table(:first_table)
   end
 end
